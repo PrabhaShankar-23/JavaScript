e223db26dc9e57e5785cce042b6da0ef703d633c
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-02-17 13:12:02 +0530

    BlackJack is now live

--- 100Apps/BlackJack Casino/main.js
+++ 100Apps/BlackJack Casino/main.js
@@ -4,12 +4,13 @@
 
 let card1 = 0;
 let card2 = 0;
-let Card_3 = 0;
+let Card_N = 0;
 
 let sum = card1 + card2;
 let message = "";
 let hasblackjack = false;
-let isalive = true;
+let gameOn = true;
+let sumN = 0;
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
@@ -17,30 +18,53 @@ let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#card-el");
 
 function startGame() {
+  // This gives us draw for the new set of cards when the player begins or starts the game.
   card1 = Math.ceil(Math.random() * 10 + 1);
   card2 = Math.ceil(Math.random() * 10 + 1);
+  sum = card1 + card2;
   if (sum <= 20) {
-    message = "Do you want to draw New card?";
+    message = "Do you want to draw New card? If 'yes' then press New Card!";
     messageEl.textContent = message;
 
     sumEl.textContent = "sum:" + (card1 + card2);
-    cardsEl.textContent = "cards:" + card1 + "*" + card2;
+    cardsEl.textContent = "Sum of Cards Value is :" + card1 + "*" + card2;
   } else if (sum === 21) {
-    message = "Got BlackJack  cheers!";
+    message = "Congratulations!!! You have got BlackJack!";
 
     messageEl.textContent = message;
-    sumEl.textContent = card1 + card2;
+    sumEl.textContent = "sum:" + card1 + card2;
     cardsEl.textContent += card1 + "**" + card2 + "--";
     hasblackjack = true;
   } else {
     message = " You are out of the game!";
-    isalive = false;
+    gameOn = false;
   }
 }
 
 function newCard() {
-  if (isalive) {
-    Card_3 = Math.ceil(Math.random() * 10 + 1);
-    sum = card1 + card2 + Card_3;
+  if (gameOn) {
+    // new card withdrawn shows this:
+    Card_N = Math.ceil(Math.random() * 10 + 1);
+    sumN = sumN + sum + Card_N;
+    if (sumN <= 20) {
+      message = "Do you want to draw New card? If 'yes' then press New Card!";
+      messageEl.textContent = message;
+      sumEl.textContent = "sum:" + sumN;
+      cardsEl.textContent += "*" + Card_N;
+    } else if (sumN === 21) {
+      message = "Congratulations!!! You have got BlackJack!";
+
+      messageEl.textContent = message;
+      sumEl.textContent = "sum:" + sumN;
+      cardsEl.textContent += "*" + Card_N;
+      hasblackjack = true;
+    } else {
+      message = " You are out of the game!";
+      messageEl.textContent = message;
+      sumEl.textContent = "sum:" + sumN;
+      cardsEl.textContent += "*" + Card_N;
+
+      gameOn = false;
+    }
   }
 }
