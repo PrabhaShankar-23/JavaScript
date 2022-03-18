0399282bfcf395b893037a74d6c2fd93f3781272
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-03-18 23:25:34 +0530

    hoisting explained

--- Daily_Drill/main.js
+++ Daily_Drill/main.js
@@ -1,2 +1,13 @@
 console.log("I woke up Early today!");
 console.log("I went straight to the Beach");
+
+var x = 7;
+
+function getname() {
+  console.log("Hello JS");
+}
+
+getname();
+
+console.log(x);
+console.log(getname);
