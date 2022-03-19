8df176b798fb4cef3411e7f927d745d3c54d5060
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-03-19 22:56:08 +0530

    closures

---
+++ Daily_Drill/hoisting.js
@@ -0,0 +1,10 @@
+var x = 7;
+
+function getname() {
+  console.log("Hello JS");
+}
+
+getname();
+
+console.log(x);
+console.log(getname);
--- Daily_Drill/main.js
+++ Daily_Drill/main.js
@@ -1,13 +1,2 @@
 console.log("I woke up Early today!");
 console.log("I went straight to the Beach");
-
-var x = 7;
-
-function getname() {
-  console.log("Hello JS");
-}
-
-getname();
-
-console.log(x);
-console.log(getname);
