e6587fed91c092c484d41bdfa5bfc81fbca28c3c
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-03-31 10:50:35 +0530

    Execution context Explored

---
+++ Daily_Drill/executionContext.js
@@ -0,0 +1,15 @@
+var a = 10;
+
+function earth() {
+  console.log("SuperMan Returns!");
+}
+console.log(moon);
+
+var moon = () => {
+  console.log("First man on MOon");
+};
+earth();
+console.log(a);
+console.log(earth);
+moon();
+console.log(moon);
