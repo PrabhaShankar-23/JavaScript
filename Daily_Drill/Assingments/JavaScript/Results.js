d0f3e75bfe6cb3b82d56ab4e96ef25c8c2f499ac
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-04-24 15:13:12 +0530

    Form for T-shirt order

---
+++ Daily_Drill/Assingments/JavaScript/Results.js
@@ -0,0 +1,29 @@
+// A function with the name calculate_results must return a result to the code that called it. Write the statement that belongs within the calculate_results function that returns the value of a variable named result from this function.
+
+// calculate_results is the function that we have defined.
+function calculate_results() {
+  let result = 2022;
+  return result;
+}
+
+// console.log is used for displaying the result.
+
+console.log(calculate_results()); // Here we are calling the calculate_results.
+
+// Another functin returing string
+function calculate_results2() {
+  let result = "SuperMan Returns!";
+  return result;
+}
+
+console.log(calculate_results2()); // Here we are calling the calculate_results2.
+
+// Another function returing a calculated value.
+function calculate_results3() {
+  const a = 10;
+  const b = 20;
+  let result = a * b;
+  return result;
+}
+
+console.log(calculate_results3()); // Here we are calling the calculate_results2. ans displaying the result.
---
+++ Daily_Drill/CoreFundamentals/JS_Play/11-25/20-25.js
@@ -0,0 +1,10 @@
+const sum = eval("10*10+5");
+console.log(sum);
+
+var num = 8;
+var num = 10;
+
+console.log(num);
+a = { key: 2 };
+console.log(JSON.stringify(a));
+console.log(a.toString());
