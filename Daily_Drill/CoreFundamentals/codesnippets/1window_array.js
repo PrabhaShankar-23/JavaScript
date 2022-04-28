67c5b63e15a28ad7c4dab02e859f3350f9560a7b
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-04-28 13:40:45 +0530

    forms: calculator & invoice total

---
+++ Daily_Drill/CoreFundamentals/codesnippets/1window_array.js
@@ -0,0 +1,15 @@
+// 1 check for array.
+let a = [1, 2, 3, 4];
+console.log(typeof a);
+
+// 2 check for array in a function
+
+function hero() {
+  let a = [1, 2, 3, 4];
+  console.log(typeof a);
+  console.log(a);
+}
+
+hero();
+// 3 check for array in a function
+console.log(0 == false);
