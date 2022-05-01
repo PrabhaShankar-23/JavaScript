e9edf7cdeafb050e6d56081e7acdaab0888618b6
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-05-01 21:15:33 +0530

    JS play for coreFundamentals

--- Daily_Drill/CoreFundamentals/JS_Play/11-25/20-25.js
+++ Daily_Drill/CoreFundamentals/JS_Play/11-25/20-25.js
@@ -1,10 +1,21 @@
+// this evaluates the expression
 const sum = eval("10*10+5");
 console.log(sum);
 
+//
 var num = 8;
 var num = 10;
 
 console.log(num);
+//
 a = { key: 2 };
 console.log(JSON.stringify(a));
 console.log(a.toString());
+
+//
+console.log("**3", 5 && 0 && 7);
+let b = [2, 3, 4, 5];
+//
+console.log("**4", typeof b);
+
+//
