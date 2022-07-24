89d60ca08e0c8ceedcebae7cb515c0ee0a27edc3
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-07-24 20:30:12 +0530

    till mobile section

--- 100Apps/writro_landing_page/src/App.js
+++ 100Apps/writro_landing_page/src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import "./style/readWrite.css"
+import "./style/read.css"
+import "./style/mobile.css"
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Read_write from './components/Read_write';
 import Read from './components/Read';
 import Footer from './components/Footer';
+import Mobile from './components/Mobile';
 
 function App() {
   return (
@@ -14,6 +17,7 @@ function App() {
       <Hero/>
       <Read_write/>
       <Read/>
+      <Mobile/>
       {/* <Footer/> */}
     </div>
   );
--- 100Apps/writro_landing_page/src/components/Read_write.js
+++ 100Apps/writro_landing_page/src/components/Read_write.js
@@ -7,13 +7,21 @@ const Read_write = () => {
         <section className='read--section'>
             <p className='writro'>Writro Writer</p>
             <p className='writeUp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum porttitor duis nec leo ac. Natoque et sapien quisque mauris tempor, sapien accumsan. Nibh metus scelerisque dolor rhoncus est amet auctor.</p>
-            <button className='btn--read--write'>Let's Write <span>&#8594;</span> </button>
+            <button className='btn--read--write'> Let's Write <span>&#8594;</span> </button>
         </section>
-        <section className='imgColumn'>
-        <img className='chart2' src=".\img\Column chart 2.png" alt="column-chart" />
-        <img className='creditCard' src=".\img\Credit card.png" alt="credit card" />
-        <img className='vector1' src=".\img\Vector (1).png" alt="credit card" />
-        <img className='vector2' src=".\img\Vector.png" alt="credit card" />
+        <section >
+        <img 
+        className='chart2' 
+        src=".\img\Column chart 2.png" alt="column-chart" />
+        <img 
+        className='creditCard' 
+        src=".\img\Credit card.png" alt="credit card" />
+        <img 
+        className='vector1' 
+        src=".\img\Vector (1).png" alt="credit card" />
+        <img 
+        className='vector2' 
+        src=".\img\Vector.png" alt="credit card" />
         </section>
 
     </div>  
