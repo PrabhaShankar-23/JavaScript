f50eff14a1c550b57c54e1610a7993a0e3f1173f
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-07-24 16:06:48 +0530

    writro section added

--- 100Apps/writro_landing_page/src/App.js
+++ 100Apps/writro_landing_page/src/App.js
@@ -1,13 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
+import "./style/readWrite.css"
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Read_write from './components/Read_write';
+import Read from './components/Read';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <Read_write/>
+      <Read/>
+      <Footer/>
     </div>
   );
 }
